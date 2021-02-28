676323cedf056586e1554134fbe692b9a42d4e19
Edracoon <77042040+Edracoon@users.noreply.github.com>
2021-02-28 02:30:50 +0100

    commit

--- ft_printf/sources/type_id.c
+++ ft_printf/sources/type_id.c
@@ -37,7 +37,7 @@ void		minus_0_di(char *nb, int di, t_flags *flags)
 
 	if (di < 0 && flags->dot >= 0)
 	{
-		ft_putchar('z');
+		ft_putchar('-');
 	}
 	else if (di < 0 && flags->dot > ft_strlen(nb))
 		ft_putchar(' ');
@@ -94,7 +94,7 @@ int			norme_di(int di, t_flags *flags)
 	}
 	if (di < 0 && (flags->dot >= 0 || flags->flag_zero == 1 || flags->flag_minus == 1))
 	{
-		if ((flags->flag_zero == 1 || flags->flag_minus == 1) && flags->dot < 0)
+		if ((flags->flag_zero == 1 || flags->flag_minus == 1) && flags->dot >= 0)
 			ft_putchar('-');
 		flags->flag_zero = 1;
 		flags->width--;
