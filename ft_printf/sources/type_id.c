52a014a6bf292d7e2b3892928786d0a733de7eb1
Edracoon <77042040+Edracoon@users.noreply.github.com>
2021-02-28 16:08:23 +0100

    commit

--- ft_printf/sources/type_id.c
+++ ft_printf/sources/type_id.c
@@ -11,7 +11,7 @@
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
-
+/*
 void		minus_1_di(char *nb, int di, t_flags *flags)
 {
 	int		temp;
@@ -62,7 +62,7 @@ void		dot_sup_0_di(int di, char *nb, t_flags *flags)
 	if (flags->dot >= 0)
 	{
 		flags->width -= flags->dot;
-		while (flags->width > 0 && (!(flags->dot < temp)))
+		while (flags->width > 0)
 		{
 			ft_putchar('b');
 			flags->width--;
@@ -104,14 +104,61 @@ int			norme_di(int di, t_flags *flags)
 	}
 	return (1);
 }
-
+*/
 void		ft_type_di(int di, t_flags *flags)
 {
 	char	*nb;
+	int		size_ap;
 
-	if (norme_di(di, flags) == 0)
-		return ;
-	if (di < 0)
+	nb = ft_itoa(di);
+	flags->width = di < 0 ? flags->width - 1 : flags->width;
+	size_ap = (flags->dot <= ft_strlen(nb) || flags->dot < 0) ? ft_strlen(nb) : flags->dot;
+	if (flags->dot == 0 && di == 0)
+		size_ap = 0;
+	if (flags->flag_minus == 0 && flags->width > size_ap)
+	{
+		if (flags->flag_zero == 1 && flags->dot < 0)
+		{
+			if (di < 0)
+				ft_putchar('-');
+			while (flags->width - size_ap > 0)
+			{
+				ft_putchar('0');
+				flags->width--;
+			}
+		}
+		else
+		{
+			while (flags->width - size_ap > 0)
+			{
+				ft_putchar(' ');
+				flags->width--;
+			}
+		}
+	}
+	if (di < 0 && (!(flags->width > size_ap && flags->flag_zero == 1
+		&& flags->dot < 0 && flags->flag_minus == 0)))
+		ft_putchar('-');
+	flags->dot = di < 0 ? flags->dot++ : flags->dot;
+	if (flags->dot > ft_strlen(nb))
+	{
+		while (flags->dot - ft_strlen(nb) > 0)
+		{
+			ft_putchar('0');
+			flags->dot--;
+		}
+	}
+	if (!(di == 0 && flags->dot == 0))
+		ft_putstr(nb, ft_strlen(nb));
+	if (flags->flag_minus > 0 && flags->width > size_ap)
+	{
+		while (flags->width - size_ap > 0)
+		{
+			ft_putchar(' ');
+			flags->width--;
+		}
+	}
+	/*if (di < 0)
 	{
 		if (di == -2147483648)
 			nb = ft_itoa(2147483648);
@@ -131,5 +178,5 @@ void		ft_type_di(int di, t_flags *flags)
 	dot_sup_0_di(di, nb, flags);
 	if (flags->flag_minus == 0)
 		minus_0_di(nb, di, flags);
-	free(nb);
+	free(nb);*/
 }
