53f8801d72f12434e03bf414d10da1b0976145f4
Edracoon <77042040+Edracoon@users.noreply.github.com>
2021-02-28 02:32:15 +0100

    commit

--- ft_printf/sources/type_id.c
+++ ft_printf/sources/type_id.c
@@ -35,7 +35,7 @@ void		minus_0_di(char *nb, int di, t_flags *flags)
 {
 	int		temp;
 
-	if (di < 0 && flags->dot >= 0)
+	if (di < 0)
 	{
 		ft_putchar('-');
 	}
@@ -94,7 +94,7 @@ int			norme_di(int di, t_flags *flags)
 	}
 	if (di < 0 && (flags->dot >= 0 || flags->flag_zero == 1 || flags->flag_minus == 1))
 	{
-		if ((flags->flag_zero == 1 || flags->flag_minus == 1) && flags->dot >= 0)
+		if ((flags->flag_zero == 1 || flags->flag_minus == 1) && flags->dot < 0)
 			ft_putchar('-');
 		flags->flag_zero = 1;
 		flags->width--;
