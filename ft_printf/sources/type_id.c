6af93b32795659f798964da31047b27f106398a2
Edracoon <77042040+Edracoon@users.noreply.github.com>
2021-02-28 18:09:27 +0100

    commit

--- ft_printf/sources/type_id.c
+++ ft_printf/sources/type_id.c
@@ -39,7 +39,7 @@ void		ft_type_di(int di, t_flags *flags)
 		if (flags->flag_zero == 1 && flags->dot < 0)
 		{
 			if (di < 0)
-				ft_putchar('a');
+				ft_putchar('-');
 			while (flags->width - size_ap > 0)
 			{
 				ft_putchar('0');
@@ -55,9 +55,9 @@ void		ft_type_di(int di, t_flags *flags)
 			}
 		}
 	}
-	if (di < 0 && (!(flags->width > size_ap
-		&& flags->dot < 0 && flags->flag_minus == 0)) && flags->dot >= 0)
-		ft_putchar('z');
+	if (di < 0 && (!(flags->width > size_ap && flags->flag_zero == 1
+		&& flags->dot < 0 && flags->flag_minus == 0)))
+		ft_putchar('-');
 	flags->dot = di < 0 ? flags->dot++ : flags->dot;
 	if (flags->dot > ft_strlen(nb))
 	{
