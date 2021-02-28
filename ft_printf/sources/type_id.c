75fa2cef40cac776bcd6f86e69732136589fdab5
Edracoon <77042040+Edracoon@users.noreply.github.com>
2021-02-28 04:10:58 +0100

    commit

--- ft_printf/sources/type_id.c
+++ ft_printf/sources/type_id.c
@@ -58,7 +58,7 @@ void		dot_sup_0_di(int di, char *nb, t_flags *flags)
 {
 	if (flags->dot >= 0)
 	{
-		if (flags->dot > flags->width)
+		if (flags->width > flags->dot)
 			flags->width--;
 		flags->width -= flags->dot;
 		while (flags->width > 0)
