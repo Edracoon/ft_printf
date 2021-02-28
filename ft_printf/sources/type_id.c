eca57ba6990f8311c1eab8138318cc386f29c3d2
Edracoon <77042040+Edracoon@users.noreply.github.com>
2021-02-28 04:18:10 +0100

    commit

--- ft_printf/sources/type_id.c
+++ ft_printf/sources/type_id.c
@@ -59,7 +59,7 @@ void		dot_sup_0_di(int di, char *nb, t_flags *flags)
 	if (flags->dot >= 0)
 	{
 		flags->width -= flags->dot;
-		while (flags->width > 0 && flags->flag_minus == 0)
+		while (flags->width > 0 && flags->dot < flags->width)
 		{
 			ft_putchar('b');
 			flags->width--;
