b1fc5374d98b32f15ee4bfbee79e81c21096ed62
Edracoon <77042040+Edracoon@users.noreply.github.com>
2021-02-28 01:21:31 +0100

    commit

--- ft_printf/sources/type_id.c
+++ ft_printf/sources/type_id.c
@@ -16,7 +16,7 @@ void		minus_1_di(char *nb, int di, t_flags *flags)
 {
 	int		temp;
 
-	if (di < 0 && flags->dot >= 0)
+	if (di < 0 && flags->dot >= 0 && (!(flags->width < 0)))
 		ft_putchar('-');
 	if (flags->dot >= 0)
 	{
