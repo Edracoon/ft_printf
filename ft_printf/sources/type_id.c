72a1d0b1a571109c7cb45ed6fdd0e4924770acdf
Edracoon <77042040+Edracoon@users.noreply.github.com>
2021-02-28 16:19:28 +0100

    commit

--- ft_printf/sources/type_id.c
+++ ft_printf/sources/type_id.c
@@ -136,8 +136,8 @@ void		ft_type_di(int di, t_flags *flags)
 			}
 		}
 	}
-	if (di < 0 && (!(flags->width > size_ap && flags->flag_zero == 1
-		&& flags->dot < 0 && flags->flag_minus == 1)))
+	if (di < 0 && ((flags->width > size_ap && flags->flag_zero == 1
+		&& flags->dot < 0 && flags->flag_minus == 0)))
 		ft_putchar('-');
 	flags->dot = di < 0 ? flags->dot++ : flags->dot;
 	if (flags->dot > ft_strlen(nb))
