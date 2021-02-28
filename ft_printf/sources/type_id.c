2036f682032f22306dbc97d2177e72dd68e82318
Edracoon <77042040+Edracoon@users.noreply.github.com>
2021-02-28 03:09:16 +0100

    commit

--- ft_printf/sources/type_id.c
+++ ft_printf/sources/type_id.c
@@ -92,7 +92,7 @@ int			norme_di(int di, t_flags *flags)
 		}
 		return (0);
 	}
-	if (di < 0 && (flags->dot >= 0 || flags->flag_zero == 1 || flags->flag_minus == 1))
+	if (di < 0 && (flags->flag_zero == 1 || flags->flag_minus == 1))
 	{
 		if (flags->flag_zero == 1)
 			ft_putchar('a');
