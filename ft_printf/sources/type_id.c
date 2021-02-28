32cb30cf4d8fcaa9e0a1f2d07387c5b8ea6ccc92
Edracoon <77042040+Edracoon@users.noreply.github.com>
2021-02-28 18:29:11 +0100

    commit

--- ft_printf/sources/type_id.c
+++ ft_printf/sources/type_id.c
@@ -55,9 +55,8 @@ void		ft_type_di(int di, t_flags *flags)
 			}
 		}
 	}
-	printf(" ici = z = %i", flags->flag_zero);
-	if (di < 0 && (!(flags->width > size_ap && flags->flag_zero == 1
-		&& flags->dot < 0 && flags->flag_minus == 0)))
+	if (di < 0 && !(flags->width > size_ap) && !(flags->flag_zero == 1)
+		&& !(flags->dot < 0) && !(flags->flag_minus == 0))
 		ft_putchar('-');
 	flags->dot = di < 0 ? flags->dot++ : flags->dot;
 	if (flags->dot > ft_strlen(nb))
